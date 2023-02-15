d796a1077aa29f344d85bb2b0d3eb46ee4b5d0ca
Thejashree <thejashreehk@gmail.com>
2023-02-15 09:30:19 +0530

    initial commit

---
+++ src/test/java/com/spring/fizzbuzz/FizzbuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
